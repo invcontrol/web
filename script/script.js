0456086db200bcc22ff9efc094067ea41120b383
Ismael <ism.concha@duocuc.cl>
2022-05-24 06:11:23 -0400

    bootstrap, validaciones, api

---
+++ script/script.js
@@ -0,0 +1,28 @@
+function getDog(){
+    $.getJSON("https://dog.ceo/api/breeds/image/random", function( data ) {
+        $(".breeds-image-random pre").html(JSON.stringify(data, null, 4));
+        $(".image-content").html("<img src='" + data.message + "'style=\"width: 50px; height: 50px\">");
+    });
+}
+
+function val() {
+    (function () {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+          .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+              if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+              }
+
+              form.classList.add('was-validated')
+            }, false)
+          })
+      })()
+}
